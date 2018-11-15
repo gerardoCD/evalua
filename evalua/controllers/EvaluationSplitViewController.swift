88388c5a1ae419c2bca728c17bc4404366b47c89
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2018-11-14 23:08:23 -0600

    add split view controllers and collapse when device is an iphone

---
+++ evalua/controllers/EvaluationSplitViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EvaluationSplitViewController.swift
+//  evalua
+//
+//  Created by Luis Ezcurdia on 11/14/18.
+//  Copyright © 2018 iOSLab FI-UNAM. All rights reserved.
+//
+
+import UIKit
+
+class EvaluationSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return UIDevice.current.userInterfaceIdiom == .phone
+    }
+}
